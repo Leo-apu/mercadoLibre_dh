8ef909eae681b397039cfc1cee132348f9cebe0e
LeoVCruz <leovcruz0304@gmail.com>
2024-01-10 09:52:34 -0300

    add web structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3300;
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+
